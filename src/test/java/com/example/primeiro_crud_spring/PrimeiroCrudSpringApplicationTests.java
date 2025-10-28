4ca858e06cc5d5c96162d5350dd656eada681af7
Pedro Luka <pedrolukagt@gmail.com>
2025-10-28 16:59:02 -0300

    first commit

---
+++ src/test/java/com/example/primeiro_crud_spring/PrimeiroCrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_crud_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroCrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
